5bb43e7eeca4dcf9ce08fa473715f94a8ba24996
Minh <minhtuan.tran@stud.uni-goettingen.de>
2026-07-07 17:37:00 +0200

    Erstes Modell

---
+++ model.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
+
+# 1. Daten laden und vorbereiten
+df = pd.read_csv("data/listings_clean.csv")
+
+# Freitext-Spalten rauswerfen (kann Modell noch nicht verarbeiten)
+df = df.drop(["name", "description", "amenities"], axis=1)
+
+# X (Features) und y (Zielvariable) trennen
+X = df.drop("price", axis=1)
+y = df["price"] 
+
+print(X.shape)
+print(y.head())
+
+# 2. Train/Test-Split
+# X_train, y_train -> 80% der Daten, zum Trainieren
+# X_test, y_test -> 20% der Daten, zum Testen
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print(f"Training:  {X_train.shape}")
+print(f"Test:      {X_test.shape}")
+
+# 3. Random Forest trainieren
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train) # Modell lernt mit den Trainingsdaten
+
+# 4. Evaluieren
+y_pred = model.predict(X_test) # Macht Vorhersage für die Testdaten
+
+mae = mean_absolute_error(y_test, y_pred) # Mean bsolute Error
+rmse = np.sqrt(mean_squared_error(y_test, y_pred)) # Root mean squared Error
+
+print(f"MAE:  {mae:.2f}€")
+print(f"RMSE: {rmse:.2f}€")
+
+print(df["price"].describe())
